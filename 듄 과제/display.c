e429c0a45299bf7e831d637ecac92951d398e7fd
최주혜 <cjg086@naver.com>
2024-11-12 21:56:36 +0900

    1)에 있는 초기상태 코드완료

--- 듄 과제/display.c
+++ 듄 과제/display.c
@@ -1,14 +1,12 @@
 #include "display.h"
 #include "io.h"
 
-// 출력할 내용들의 좌상단(topleft) 좌표
+// 좌상단 좌표
 const POSITION resource_pos = { 0, 0 };
 const POSITION map_pos = { 1, 0 };
-
-// 좌표 수정: 시스템 메시지와 명령 창을 원하는 위치에 맞게 조정
 const POSITION system_message_pos = { MAP_HEIGHT + 1, 0 };
-const POSITION command_window_pos = { MAP_HEIGHT + 1,MAP_WIDTH + 5 }; // 오른쪽 하단 명령창
-const POSITION status_window_pos = { 0, MAP_WIDTH + 5 }; // 오른쪽 상단 상태창
+const POSITION command_window_pos = { MAP_HEIGHT + 1, MAP_WIDTH + 5 };
+const POSITION status_window_pos = { 0, MAP_WIDTH + 5 };
 
 char backbuf[MAP_HEIGHT][MAP_WIDTH] = { 0 };
 char frontbuf[MAP_HEIGHT][MAP_WIDTH] = { 0 };
@@ -23,31 +21,120 @@ void display(
     char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
     CURSOR cursor)
 {
-    // 자원 상태 창
-    set_color(COLOR_RESOURCE);
-    gotoxy(resource_pos);
-    printf("spice = %d/%d, population = %d/%d\n",
-        resource.spice, resource.spice_max,
-        resource.population, resource.population_max);
-
-    // 맵 창
+    display_resource(resource);
     display_map(map);
     display_cursor(cursor);
 
-    // 시스템 메시지 창 (왼쪽 하단)
     gotoxy(system_message_pos);
     printf("[System Message]: ...");
 
-    // 명령 창 (오른쪽 하단)
     gotoxy(command_window_pos);
     printf("[Commands]: ...");
 
-    // 상태 창 (오른쪽 상단)
     gotoxy(status_window_pos);
     printf("[Status]: Selected unit info ...");
 }
 
-// subfunction of draw_map()
+void display_resource(RESOURCE resource) {
+    // 플레이어 본부 B (좌하단)
+    set_color(COLOR_B);
+    gotoxy(padd(resource_pos, (POSITION) { 17, 1 }));
+    printf("B");
+    gotoxy(padd(resource_pos, (POSITION) { 17, 2 }));
+    printf("B");
+    gotoxy(padd(resource_pos, (POSITION) { 16, 1 }));
+    printf("B");
+    gotoxy(padd(resource_pos, (POSITION) { 16, 2 }));
+    printf("B");
+
+    // 플레이어 하베스터 H (좌하단)
+    set_color(COLOR_B);
+    gotoxy(padd(resource_pos, (POSITION) { 15, 1 }));
+    printf("H");
+
+    // 장판 P (좌하단, 2x2)
+    set_color(COLOR_P);
+    gotoxy(padd(resource_pos, (POSITION) { 16, 3 }));
+    printf("P");
+    gotoxy(padd(resource_pos, (POSITION) { 16, 4 }));
+    printf("P");
+    gotoxy(padd(resource_pos, (POSITION) { 17, 3 }));
+    printf("P");
+    gotoxy(padd(resource_pos, (POSITION) { 17, 4 }));
+    printf("P");
+
+    // 적 본부 B (우상단)
+    set_color(COLOR_BB);
+    gotoxy(padd(resource_pos, (POSITION) { 2, 57 }));
+    printf("B");
+    gotoxy(padd(resource_pos, (POSITION) { 3, 57 }));
+    printf("B");
+    gotoxy(padd(resource_pos, (POSITION) { 2, 58 }));
+    printf("B");
+    gotoxy(padd(resource_pos, (POSITION) { 3, 58 }));
+    printf("B");
+    // 장판 P (우상단, 2x2)
+    set_color(COLOR_P);
+    gotoxy(padd(resource_pos, (POSITION) { 2, 55 }));
+    printf("P");
+    gotoxy(padd(resource_pos, (POSITION) { 3, 55 }));
+    printf("P");
+    gotoxy(padd(resource_pos, (POSITION) { 2, 56 }));
+    printf("P");
+    gotoxy(padd(resource_pos, (POSITION) { 3, 56 }));
+    printf("P");
+
+    // 적 하베스터 H (우상단)
+    set_color(COLOR_BB);
+    gotoxy(padd(resource_pos, (POSITION) { 4, 58 }));
+    printf("H");
+
+    // 스파이스 매장지 5
+    set_color(COLOR_5);
+    gotoxy(padd(resource_pos, (POSITION) { 13, 1 }));
+    printf("5");
+    gotoxy(padd(resource_pos, (POSITION) { 6, 58 }));
+    printf("5");
+
+    // 샌드웜 W
+    set_color(COLOR_W);
+    gotoxy(padd(resource_pos, (POSITION) { 3, 9 }));
+    printf("W");
+    gotoxy(padd(resource_pos, (POSITION) { 14, 44 }));
+    printf("W");
+
+    // 바위 R (2x2 및 1x1)
+    set_color(COLOR_R);
+    gotoxy(padd(resource_pos, (POSITION) { 6, 19 }));
+    printf("R");
+    gotoxy(padd(resource_pos, (POSITION) { 6, 20 }));
+    printf("R");
+    gotoxy(padd(resource_pos, (POSITION) { 7, 19 }));
+    printf("R");
+    gotoxy(padd(resource_pos, (POSITION) { 7, 20 }));
+    printf("R");
+    //
+    gotoxy(padd(resource_pos, (POSITION) { 15, 28 }));
+    printf("R");
+    gotoxy(padd(resource_pos, (POSITION) { 15, 27 }));
+    printf("R");
+    gotoxy(padd(resource_pos, (POSITION) { 16, 28 }));
+    printf("R");
+    gotoxy(padd(resource_pos, (POSITION) { 16, 27 }));
+    printf("R");
+    //
+    gotoxy(padd(resource_pos, (POSITION) { 13, 14 }));
+    printf("R");
+    //
+    gotoxy(padd(resource_pos, (POSITION) { 15, 54 }));
+    printf("R");
+
+    gotoxy(padd(resource_pos, (POSITION) { 5, 52 }));
+    printf("R");
+
+    set_color(COLOR_DEFAULT);
+}
+
 void project(char src[N_LAYER][MAP_HEIGHT][MAP_WIDTH], char dest[MAP_HEIGHT][MAP_WIDTH]) {
     for (int i = 0; i < MAP_HEIGHT; i++) {
         for (int j = 0; j < MAP_WIDTH; j++) {
@@ -74,7 +161,6 @@ void display_map(char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH]) {
     }
 }
 
-// frontbuf[][]에서 커서 위치의 문자를 색만 바꿔서 그대로 다시 출력
 void display_cursor(CURSOR cursor) {
     POSITION prev = cursor.previous;
     POSITION curr = cursor.current;
--- 듄 과제/display.h
+++ 듄 과제/display.h
@@ -1,26 +1,28 @@
-/*
-*  display.c:
-* 화면에 게임 정보를 출력
-* 맵, 커서, 시스템 메시지, 정보창, 자원 상태 등등
-* io.c에 있는 함수들을 사용함
-*/
-
 #ifndef _DISPLAY_H_
 #define _DISPLAY_H_
 
 #include "common.h"
 
-// 표시할 색상 정의. 대충 맞춰 뒀는데, 취향껏 추가하거나 변경하기
-#define COLOR_DEFAULT	15
-#define COLOR_CURSOR	112
-#define COLOR_RESOURCE  112
+#define COLOR_DEFAULT    15
+#define COLOR_CURSOR     112
+#define COLOR_RESOURCE   112
+#define COLOR_R          8   // Grey color
+#define COLOR_W          14  // Yellow color
+#define COLOR_P          8   // Dark Grey color
+#define COLOR_B          9   // Blue color
+#define COLOR_BB         4   // Blue color
+#define COLOR_H          1   // Light Blue color
+#define COLOR_5          12  // Orange color
 
-// 지금은 자원, 맵, 커서만 표시
-// 앞으로 화면에 표시할 내용들 여기에 추가하기
+extern const POSITION resource_pos;
+extern const POSITION system_message_pos;
+extern const POSITION command_window_pos;
+extern const POSITION status_window_pos;
+
 void display(
-	RESOURCE resource,
-	char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
-	CURSOR cursor
+    RESOURCE resource,
+    char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
+    CURSOR cursor
 );
 
 #endif
